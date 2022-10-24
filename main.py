be8042405f97d8fde742d62696a8dfa3127024e9
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-24 13:15:37 +0300

    Check if guess is in states list, write correct guesses onto the map, use loop to keep guessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,11 +7,23 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+quiz_on = True
+correct_guesses = 0
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+while quiz_on:
 
-
-
+    answer_state = screen.textinput(title=f"{correct_guesses}/50 Guess the State", prompt="What's another state's name?").title()
+    states = pandas.read_csv('50_states.csv')
+    if answer_state in states.values:
+        coor = states[states.state == answer_state]
+        x_coor = coor.iloc[0, 1]
+        y_coor = coor.iloc[0, 2]
+        state_name = turtle.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        state_name.goto(x_coor, y_coor)
+        state_name.write(answer_state)
+        correct_guesses += 1
 
 
 
@@ -20,4 +33,4 @@ answer_state = screen.textinput(title="Guess the State", prompt="What's another 
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 #
-# turtle.mainloop()
+turtle.mainloop()
